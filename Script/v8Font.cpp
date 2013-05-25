4787fe8cb497b2d5db620309f2c02a691b8fe5ec
Ruben Rodriguez II <cha0s@therealcha0s.net>
2013-05-25 01:10:37 -0500

    Node-style callback signature for font loading

--- Script/v8Font.cpp
+++ Script/v8Font.cpp
@@ -117,15 +117,18 @@ v8::Handle<v8::Value> v8Font::Load(const v8::Arguments &args) {
 		);
 
 		Handle<Value> argv[] = {
+			Null(),
 			font
 		};
-		fn->Call(fn, 1, argv);
+		fn->Call(fn, 2, argv);
 	}
 	catch (std::exception &e) {
 
-		return ThrowException(
-			v8::Exception::Error(String::New(e.what()))
-		);
+		Handle<Value> argv[] = {
+			v8::Exception::Error(String::New(e.what())),
+			Null()
+		};
+		fn->Call(fn, 2, argv);
 	}
 
 	return Undefined();
